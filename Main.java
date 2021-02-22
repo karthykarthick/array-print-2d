c47313f9fece609c3a42e49b19c00e6d0539847a
karthick <karthylock@gmail.com>
2021-02-22 16:06:05 +0000

    added comment

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 class Main {  
   public static void main(String args[]) { 
+    // initialize the array
     int arr[][] =  {{1,2,3},{4,5,6},{7,8,9}};
+    // loop through the array row
     for(int i = 0; i< arr.length; i++){
         for(int j = 0;  j< arr.length; j++){
             System.out.println(arr[i][j]);
