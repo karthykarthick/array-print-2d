e67a844f8e4150efe4a187c1fa886cabf7e5141f
karthick <karthylock@gmail.com>
2021-02-22 16:05:10 +0000

    i add changes to the code

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ class Main {
   public static void main(String args[]) { 
     int arr[][] =  {{1,2,3},{4,5,6},{7,8,9}};
     for(int i = 0; i< arr.length; i++){
-        for(int j = 0 ;  j< arr.length; j++){
+        for(int j = 0;  j< arr.length; j++){
             System.out.println(arr[i][j]);
         }
     }
